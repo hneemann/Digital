f6c7014f1b67751a45d711ac95e0ac99ae0f94a3
hneemann <helmut.neemann@arcor.de>
2016-04-06 14:31:52 +0200

    made DataSet a Drawable

--- src/main/java/de/neemann/digital/gui/components/data/DataSet.java
+++ src/main/java/de/neemann/digital/gui/components/data/DataSet.java
@@ -1,6 +1,11 @@
 package de.neemann.digital.gui.components.data;
 
 import de.neemann.digital.core.Model;
+import de.neemann.digital.draw.graphics.Graphic;
+import de.neemann.digital.draw.graphics.Orientation;
+import de.neemann.digital.draw.graphics.Style;
+import de.neemann.digital.draw.graphics.Vector;
+import de.neemann.digital.draw.shapes.Drawable;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -11,10 +16,11 @@ import java.util.Iterator;
  *
  * @author hneemann
  */
-public class DataSet implements Iterable<DataSample> {
+public class DataSet implements Iterable<DataSample>, Drawable {
     private static final int MAX_SAMPLES = 1000;
     private final ArrayList<Model.Signal> signals;
     private final ArrayList<DataSample> samples;
+    private final int maxTextLength;
     private DataSample min;
     private DataSample max;
 
@@ -26,6 +32,13 @@ public class DataSet implements Iterable<DataSample> {
     public DataSet(ArrayList<Model.Signal> signals) {
         this.signals = signals;
         samples = new ArrayList<>();
+        int tl = 0;
+        for (int i = 0; i < signalSize(); i++) {
+            String text = getSignal(i).getName();
+            int w = text.length();
+            if (w > tl) tl = w;
+        }
+        maxTextLength = tl;
     }
 
     /**
@@ -101,5 +114,63 @@ public class DataSet implements Iterable<DataSample> {
      */
     public Model.Signal getSignal(int i) {
         return signals.get(i);
+    }
+
+
+    private static final int BORDER = 10;
+    private static final int SIZE = 25;
+    private static final int SEP2 = 5;
+    private static final int SEP = SEP2 * 2;
+
+
+    @Override
+    public void drawTo(Graphic g, boolean highLight) {
+        int x = getTextBorder();
+
+        int yOffs = SIZE / 2;
+        int y = BORDER;
+        for (int i = 0; i < signalSize(); i++) {
+            String text = getSignal(i).getName();
+            g.drawText(new Vector(x - 2, y + yOffs), new Vector(x + 1, y + yOffs), text, Orientation.RIGHTCENTER, Style.NORMAL);
+            g.drawLine(new Vector(x, y - SEP2), new Vector(x + SIZE * size(), y - SEP2), Style.DASH);
+            y += SIZE + SEP;
+        }
+        g.drawLine(new Vector(x, y - SEP2), new Vector(x + SIZE * size(), y - SEP2), Style.DASH);
+
+
+        int[] lastRy = new int[signalSize()];
+        boolean first = true;
+        for (DataSample s : this) {
+            g.drawLine(new Vector(x, BORDER - SEP2), new Vector(x, (SIZE + SEP) * signalSize() + BORDER - SEP2), Style.DASH);
+            y = BORDER;
+            for (int i = 0; i < signalSize(); i++) {
+
+                long width = getWidth(i);
+                if (width == 0) width = 1;
+                //int ry = (int) (SIZE-(SIZE*(s.getValue(i)-dataSet.getMin().getValue(i)))/ width);
+                int ry = (int) (SIZE - (SIZE * s.getValue(i)) / width);
+                g.drawLine(new Vector(x, y + ry), new Vector(x + SIZE, y + ry), Style.NORMAL);
+                if (!first && ry != lastRy[i])
+                    g.drawLine(new Vector(x, y + lastRy[i]), new Vector(x, y + ry), Style.NORMAL);
+
+                lastRy[i] = ry;
+                y += SIZE + SEP;
+            }
+            first = false;
+            x += SIZE;
+        }
+        g.drawLine(new Vector(x, BORDER - SEP2), new Vector(x, (SIZE + SEP) * signalSize() + BORDER - SEP2), Style.DASH);
+    }
+
+    private int getTextBorder() {
+        return maxTextLength * Style.NORMAL.getFontSize() / 2 + BORDER + SEP;
+    }
+
+    public int getGraphicWidth() {
+        return getTextBorder() + size() * SIZE;
+    }
+
+    public int getGraphicHeight() {
+        return signalSize() * (SIZE + SEP) + 2 * BORDER;
     }
 }
