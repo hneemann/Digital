7e49ee85f52649dc91244ddc9307061fcd271fc3
hneemann <helmut.neemann@arcor.de>
2016-04-16 16:12:27 +0200

    added integration tests for SVG export

--- src/test/java/de/neemann/digital/integration/TestExport.java
+++ src/test/java/de/neemann/digital/integration/TestExport.java
@@ -3,15 +3,14 @@ package de.neemann.digital.integration;
 import de.neemann.digital.core.NodeException;
 import de.neemann.digital.draw.elements.Circuit;
 import de.neemann.digital.draw.elements.PinException;
-import de.neemann.digital.draw.graphics.GraphicMinMax;
-import de.neemann.digital.draw.graphics.GraphicSVGIndex;
-import de.neemann.digital.draw.graphics.GraphicSVGLaTeX;
+import de.neemann.digital.draw.graphics.*;
 import de.neemann.digital.draw.graphics.linemerger.GraphicLineCollector;
 import de.neemann.digital.draw.graphics.linemerger.GraphicSkipLines;
 import junit.framework.TestCase;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * Only checks that something is written without an error
@@ -20,37 +19,39 @@ import java.io.IOException;
  */
 public class TestExport extends TestCase {
 
-    public void testSVGExport() throws NodeException, PinException, IOException {
-        Circuit circuit = new ToBreakRunner("dig/processor/Processor_fibonacci.dig").getCircuit();
+    private interface Creator {
+        Graphic create(OutputStream out, Vector min, Vector max) throws IOException;
+    }
+
+    private static ByteArrayOutputStream export(String file, Creator creator) throws NodeException, PinException, IOException {
+        Circuit circuit = new ToBreakRunner(file).getCircuit();
 
         GraphicMinMax minMax = new GraphicMinMax();
         circuit.drawTo(minMax);
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        GraphicSVGIndex gr = new GraphicSVGIndex(baos, minMax.getMin(), minMax.getMax(), null, 15);
+        Graphic gr = creator.create(baos, minMax.getMin(), minMax.getMax());
 
         GraphicLineCollector glc = new GraphicLineCollector();
         circuit.drawTo(glc);
         glc.drawTo(gr);
 
         circuit.drawTo(new GraphicSkipLines(gr));
+        return baos;
+    }
+
+    public void testSVGExport() throws NodeException, PinException, IOException {
+        ByteArrayOutputStream baos
+                = export("dig/processor/Processor_fibonacci.dig",
+                (out, min, max) -> new GraphicSVGIndex(out, min, max, null, 15));
 
         assertTrue(baos.size() > 15000);
     }
 
     public void testSVGExportLaTeX() throws NodeException, PinException, IOException {
-        Circuit circuit = new ToBreakRunner("dig/processor/Processor_fibonacci.dig").getCircuit();
-
-        GraphicMinMax minMax = new GraphicMinMax();
-        circuit.drawTo(minMax);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        GraphicSVGLaTeX gr = new GraphicSVGLaTeX(baos, minMax.getMin(), minMax.getMax(), null, 15);
-
-        GraphicLineCollector glc = new GraphicLineCollector();
-        circuit.drawTo(glc);
-        glc.drawTo(gr);
-
-        circuit.drawTo(new GraphicSkipLines(gr));
+        ByteArrayOutputStream baos
+                = export("dig/processor/Processor_fibonacci.dig",
+                (out, min, max) -> new GraphicSVGLaTeX(out, min, max, null, 15));
 
         assertTrue(baos.size() > 8000);
     }
