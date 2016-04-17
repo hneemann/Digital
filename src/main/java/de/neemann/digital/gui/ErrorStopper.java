9c3324752d2b5cf21a7eb04796a1c84d724e331b
hneemann <helmut.neemann@arcor.de>
2016-04-17 15:12:04 +0200

    added some documentation

--- src/main/java/de/neemann/digital/gui/ErrorStopper.java
+++ src/main/java/de/neemann/digital/gui/ErrorStopper.java
@@ -1,8 +1,16 @@
 package de.neemann.digital.gui;
 
 /**
+ * Interface used to stop the model.
+ *
  * @author hneemann
  */
 public interface ErrorStopper {
+    /**
+     * Called to stop the mode
+     *
+     * @param message the message
+     * @param cause   the cause
+     */
     void showErrorAndStopModel(String message, Exception cause);
 }
