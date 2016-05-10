11be8f461bb25dea67a864baf0171e38a6e7d954
hneemann <helmut.neemann@arcor.de>
2016-05-10 18:50:28 +0200

    added example Comperator 7485

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -18,7 +18,7 @@ public class TestExamples extends TestCase {
     private static final File examples=new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
 
     public void testCombinatorial() throws Exception {
-        assertEquals(26, scan(new File(examples, "combinatorial")));
+        assertEquals(28, scan(new File(examples, "combinatorial")));
     }
 
     public void testSequential() throws Exception {
