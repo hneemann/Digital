730842e66b27df511af757edaf7e1d10cd755b4a
hneemann <helmut.neemann@arcor.de>
2016-05-23 13:17:46 +0200

    added a test for the CUPL builder

--- src/test/java/de/neemann/digital/draw/builder/CuplCreatorTest.java
+++ src/test/java/de/neemann/digital/draw/builder/CuplCreatorTest.java
@@ -1,12 +1,7 @@
 package de.neemann.digital.draw.builder;
 
-import de.neemann.digital.TestExecuter;
 import de.neemann.digital.analyse.expression.Expression;
 import de.neemann.digital.analyse.expression.Variable;
-import de.neemann.digital.draw.elements.Circuit;
-import de.neemann.digital.draw.library.ElementLibrary;
-import de.neemann.digital.draw.model.ModelDescription;
-import de.neemann.digital.draw.shapes.ShapeFactory;
 import junit.framework.TestCase;
 
 import java.io.ByteArrayOutputStream;
@@ -19,55 +14,7 @@ import static de.neemann.digital.analyse.expression.Operation.or;
 /**
  * @author hneemann
  */
-public class BuilderTest extends TestCase {
-
-    public void testBuilderCombinatorial() throws Exception {
-
-        Variable a = new Variable("a");
-        Variable b = new Variable("b");
-
-        // xor
-        Expression y = and(or(a, b), not(and(a, b)));
-
-        ElementLibrary library = new ElementLibrary();
-        Circuit circuit = new CircuitBuilder(new ShapeFactory(library))
-                .addExpression("y", y)
-                .createCircuit();
-
-        ModelDescription m = new ModelDescription(circuit, library);
-
-        TestExecuter te = new TestExecuter(m.createModel(false)).setUp(m);
-        te.check(0, 0, 0);
-        te.check(0, 1, 1);
-        te.check(1, 0, 1);
-        te.check(1, 1, 0);
-    }
-
-    public void testBuilderSequential() throws Exception {
-        Variable y0 = new Variable("Y_0");
-        Variable y1 = new Variable("Y_1");
-
-        // counter
-        Expression y0s = not(y0);
-        Expression y1s = or(and(not(y0), y1), and(y0, not(y1)));
-
-        ElementLibrary library = new ElementLibrary();
-        Circuit circuit = new CircuitBuilder(new ShapeFactory(library))
-                .addState("Y_0", y0s)
-                .addState("Y_1", y1s)
-                .addExpression("Y_0", y0)
-                .addExpression("Y_1", y1)
-                .createCircuit();
-
-        ModelDescription m = new ModelDescription(circuit, library);
-        TestExecuter te = new TestExecuter(m.createModel(false)).setUp(m);
-        te.check(0, 0);
-        te.checkC(1, 0);
-        te.checkC(0, 1);
-        te.checkC(1, 1);
-        te.checkC(0, 0);
-    }
-
+public class CuplCreatorTest extends TestCase {
     public void testCUPLBuilder() throws Exception {
         Variable y0 = new Variable("Y_0");
         Variable y1 = new Variable("Y_1");
@@ -108,8 +55,8 @@ public class BuilderTest extends TestCase {
                 "A = Y_0 & Y_1 ;\r\n", baos.toString());
     }
 
-    public void testCUPLBuilderVars() throws Exception {
-        Variable y0 = new Variable("D");
+    public void testCUPLBuilderInvalidVars() throws Exception {
+        Variable y0 = new Variable("D");  // D is not allowed in CUPL
         Variable y1 = new Variable("Y_1");
 
         // counter
@@ -127,6 +74,5 @@ public class BuilderTest extends TestCase {
             assertTrue(true);
         }
     }
-
 
 }
