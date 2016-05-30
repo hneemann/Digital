23855c78b2edb27b563db298a5891ba2c1a58dfd
helmut.neemann <helmut.neemann@arcor.de>
2016-05-30 10:36:40 +0200

    added an example

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -22,7 +22,7 @@ public class TestExamples extends TestCase {
     }
 
     public void testSequential() throws Exception {
-        assertEquals(20, scan(new File(examples, "sequential")));
+        assertEquals(21, scan(new File(examples, "sequential")));
     }
 
     public void testProcessor() throws Exception {
