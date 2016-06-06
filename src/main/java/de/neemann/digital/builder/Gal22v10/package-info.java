bbbfdbee1e5a3b905948c4acd7ebab538c949d4f
hneemann <helmut.neemann@arcor.de>
2016-06-06 18:16:44 +0200

    fixed some checkstyle issues

---
+++ src/main/java/de/neemann/digital/builder/Gal22v10/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Classes to handle a GAL22v10.
+ * Allows the simulator to directly create a JEDEC file which you can use
+ * to program a GAL22v10. So it is not necessary to use a special compiler.
+ *
+ * @author hneemann
+ */
+package de.neemann.digital.builder.Gal22v10;
