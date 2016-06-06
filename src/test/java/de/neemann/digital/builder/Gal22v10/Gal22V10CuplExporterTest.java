093502c69e30a3e274973998303552a2242e662c
hneemann <helmut.neemann@arcor.de>
2016-06-06 17:15:36 +0200

    added test for GAL22v10 CUPL export

---
+++ src/test/java/de/neemann/digital/builder/Gal22v10/Gal22V10CuplExporterTest.java
@@ -0,0 +1,70 @@
+package de.neemann.digital.builder.Gal22v10;
+
+import de.neemann.digital.analyse.expression.Expression;
+import de.neemann.digital.analyse.expression.Variable;
+import junit.framework.TestCase;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Date;
+
+import static de.neemann.digital.analyse.expression.Not.not;
+import static de.neemann.digital.analyse.expression.Operation.and;
+import static de.neemann.digital.analyse.expression.Operation.or;
+
+/**
+ * @author hneemann
+ */
+public class Gal22V10CuplExporterTest extends TestCase {
+
+    public void testCUPLExporter() throws Exception {
+        Variable y0 = new Variable("Y_0");
+        Variable y1 = new Variable("Y_1");
+
+        // counter
+        Expression y0s = not(y0);
+        Expression y1s = or(and(not(y0), y1), and(y0, not(y1)));
+
+        Gal22v10CuplExporter ce = new Gal22v10CuplExporter("user", new Date(0));
+        ce.setProjectName("test");
+        ce.getBuilder()
+                .addSequential("Y_0", y0s)
+                .addSequential("Y_1", y1s)
+                .addCombinatorial("A", and(y0, y1));
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ce.writeTo(baos);
+
+        assertEquals("Name     test ;\r\n" +
+                "PartNo   00 ;\r\n" +
+                "Date     01.01.1970 ;\r\n" +
+                "Revision 01 ;\r\n" +
+                "Designer user ;\r\n" +
+                "Company  unknown ;\r\n" +
+                "Assembly None ;\r\n" +
+                "Location unknown ;\r\n" +
+                "Device   g22v10 ;\r\n" +
+                "\r\n" +
+                "ar = 'b'0 ;\r\n" +
+                "sp = 'b'0 ;\r\n" +
+                "\r\n" +
+                "/* inputs */\r\n" +
+                "PIN 1 = CLK;\r\n" +
+                "\r\n" +
+                "/* outputs */\r\n" +
+                "PIN 14 = Y_0;\r\n" +
+                "PIN 15 = Y_1;\r\n" +
+                "PIN 16 = A;\r\n" +
+                "\r\n" +
+                "/* sequential logic */\r\n" +
+                "Y_0.D = !Y_0;\r\n" +
+                "Y_0.ar = ar ;\r\n" +
+                "Y_0.sp = sp ;\r\n" +
+                "Y_1.D = (!Y_0 & Y_1) # (Y_0 & !Y_1);\r\n" +
+                "Y_1.ar = ar ;\r\n" +
+                "Y_1.sp = sp ;\r\n" +
+                "\r\n" +
+                "/* combinatorial logic */\r\n" +
+                "A = Y_0 & Y_1;\r\n", baos.toString());
+    }
+
+}
