e0aba6cefa5e9ce1f7670f64f377d570c406ac21
hneemann <helmut.neemann@arcor.de>
2016-06-17 17:08:25 +0200

    make circuits transparent

--- src/main/java/de/neemann/digital/core/element/Keys.java
+++ src/main/java/de/neemann/digital/core/element/Keys.java
@@ -58,7 +58,7 @@ public final class Keys {
      * Background Color of nested circuits
      */
     public static final Key<java.awt.Color> BACKGROUND_COLOR
-            = new Key<>("Color", new Color(255, 255, 210));
+            = new Key<>("Color", new Color(255, 255, 0, 64));
 
     /**
      * The input splitting of a splitter
