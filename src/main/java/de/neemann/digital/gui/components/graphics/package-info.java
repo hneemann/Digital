efadc7d567fd11027f22c7a8c57369c9882f6a66
hneemann <helmut.neemann@arcor.de>
2016-07-08 19:56:20 +0200

    first working graphics card

--- src/main/java/de/neemann/digital/gui/components/graphics/package-info.java
+++ src/main/java/de/neemann/digital/gui/components/graphics/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Implements a simple graphics card
  * The graphics card behaves like a single port RAM. You can set the size of the graphics in pixels.
- * Every pixel is represented by one byte in teh graphics RAM.
+ * Every pixel is represented by one byte in the graphics RAM.
  * The RAM is twice als large as needed. So two banks are supported to allow double buffering.
  * The bank, which is shown is selected by an additional input bit.
  *
