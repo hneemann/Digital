599a9e5e1d237321aa4df8a12d1a5ec853ed4438
hneemann <helmut.neemann@arcor.de>
2016-07-08 19:56:11 +0200

    first working graphics card

--- src/main/java/de/neemann/digital/gui/components/graphics/GraphicDialog.java
+++ src/main/java/de/neemann/digital/gui/components/graphics/GraphicDialog.java
@@ -6,11 +6,19 @@ import de.neemann.digital.lang.Lang;
 import javax.swing.*;
 
 /**
+ * The dialog used to show the graphics
+ *
  * @author hneemann
  */
 public class GraphicDialog extends JDialog {
     private final GraphicComponent graphicComponent;
 
+    /**
+     * Creates a new instance of the given size
+     *
+     * @param width  width in pixel
+     * @param height height in pixel
+     */
     public GraphicDialog(int width, int height) {
         super((JFrame) null, Lang.get("elem_GraphicCard"), false);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -24,6 +32,12 @@ public class GraphicDialog extends JDialog {
         setVisible(true);
     }
 
+    /**
+     * Updates the graphics data
+     *
+     * @param memory the raw data to use
+     * @param bank   the bank to show
+     */
     public void updateGraphic(DataField memory, boolean bank) {
         graphicComponent.updateGraphic(memory.getData(), bank);
     }
