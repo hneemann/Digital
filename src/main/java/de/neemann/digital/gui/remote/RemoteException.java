26ac2c5fb304a7e2afa294aa3047b00994d9ae71
hneemann <helmut.neemann@arcor.de>
2016-08-20 13:21:12 +0200

    added javadoc

--- src/main/java/de/neemann/digital/gui/remote/RemoteException.java
+++ src/main/java/de/neemann/digital/gui/remote/RemoteException.java
@@ -1,9 +1,15 @@
 package de.neemann.digital.gui.remote;
 
 /**
+ * Exception thrown by the RemoteInterface
  * Created by hneemann on 20.08.16.
  */
 public class RemoteException extends Exception {
+    /**
+     * Create a new Exception
+     *
+     * @param message the message
+     */
     public RemoteException(String message) {
         super(message);
     }
