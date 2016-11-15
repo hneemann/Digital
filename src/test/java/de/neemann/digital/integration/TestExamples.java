ad1775fbd9b44111bd4978676f8a8a0e9a5337bd
hneemann <helmut.neemann@arcor.de>
2016-11-15 07:53:13 +0100

    Added test of antiparallel diodes

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -37,8 +37,8 @@ public class TestExamples extends TestCase {
      */
     public void testTestExamples() throws Exception {
         File examples = new File(Resources.getRoot(), "/dig/test");
-        assertEquals(11, new FileScanner(this::check).scan(examples));
-        assertEquals(11,testCasesInFiles);
+        assertEquals(12, new FileScanner(this::check).scan(examples));
+        assertEquals(12,testCasesInFiles);
     }
 
 
