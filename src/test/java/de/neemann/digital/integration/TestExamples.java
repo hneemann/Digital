bf73f449269383711808700939fe5b5f8befaa28
hneemann <helmut.neemann@arcor.de>
2016-11-16 21:52:04 +0100

    Added comments and added the new discrete.dig file also in integration test.

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -27,7 +27,7 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(87, new FileScanner(this::check).scan(examples));
+        assertEquals(88, new FileScanner(this::check).scan(examples));
         assertEquals(17,testCasesInFiles);
     }
 
