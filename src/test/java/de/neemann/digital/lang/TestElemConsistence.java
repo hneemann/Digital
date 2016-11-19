919a44be24f3a001c4e75bb7d71be83f40b64973
hneemann <helmut.neemann@arcor.de>
2016-11-19 13:16:50 +0100

    more special cases a handled correctly

--- src/test/java/de/neemann/digital/lang/TestElemConsistence.java
+++ src/test/java/de/neemann/digital/lang/TestElemConsistence.java
@@ -3,6 +3,8 @@ package de.neemann.digital.lang;
 import de.neemann.digital.core.NodeException;
 import de.neemann.digital.core.basic.FanIn;
 import de.neemann.digital.core.element.*;
+import de.neemann.digital.core.memory.LookUpTable;
+import de.neemann.digital.core.wiring.Splitter;
 import de.neemann.digital.draw.library.ElementLibrary;
 import junit.framework.TestCase;
 
@@ -28,7 +30,7 @@ public class TestElemConsistence extends TestCase {
             if (Lang.getNull(key + "_tt") == null)
                 System.out.println("missing key " + key + "_tt");
 
-            if (!(etd.createElement(new ElementAttributes()) instanceof FanIn)) {
+            if (isNormalElement(etd)) {
                 PinDescriptions inputs = etd.getInputDescription(new ElementAttributes());
                 for (PinDescription in : inputs) {
                     final String inputKey = key + "_pin_" + in.getName();
@@ -40,6 +42,14 @@ public class TestElemConsistence extends TestCase {
                 }
             }
         }
+    }
+
+    private boolean isNormalElement(ElementTypeDescription etd) {
+        Element e = etd.createElement(new ElementAttributes());
+
+        return !(e instanceof FanIn
+                || e instanceof Splitter
+                || e instanceof LookUpTable);
     }
 
 }
