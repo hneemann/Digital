ba8c3dc60e9beb1e96dd4d09cd049627c8af37b8
hneemann <helmut.neemann@arcor.de>
2016-11-26 21:22:48 +0100

    typo

--- src/main/java/de/neemann/digital/core/package-info.java
+++ src/main/java/de/neemann/digital/core/package-info.java
@@ -3,7 +3,7 @@
  * So all the logic gates, memory and so on are implemented here.
  * This core model does not contain any information about its graphical representation.
  * But every single value is represented by the class {@link de.neemann.digital.core.ObservableValue}
- * an the graphical representation registers listeners tho the values to be notified if a state
+ * and the graphical representation registers listeners tho the values to be notified if a state
  * change occurred, and maybe a repaint is necessary.
  *
  * @author hneemann
