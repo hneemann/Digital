bb432d07bb9c25b1a5b99f86eb43fb6afdb020d8
hneemann <helmut.neemann@arcor.de>
2016-11-29 20:42:49 +0100

    added an additional test for #10

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -37,8 +37,8 @@ public class TestExamples extends TestCase {
      */
     public void testTestExamples() throws Exception {
         File examples = new File(Resources.getRoot(), "/dig/test");
-        assertEquals(22, new FileScanner(this::check).scan(examples));
-        assertEquals(17,testCasesInFiles);
+        assertEquals(24, new FileScanner(this::check).scan(examples));
+        assertEquals(18,testCasesInFiles);
     }
 
 
