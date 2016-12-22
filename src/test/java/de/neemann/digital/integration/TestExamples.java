0c7be6ddc7a8ba72db36be3034ee63fdfa999f15
helmut.neemann <helmut.neemann@arcor.de>
2016-12-22 08:44:55 +0100

    used separate ControlUnit for Processor with Interrupt-Controller

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -28,7 +28,7 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(92, new FileScanner(this::check).scan(examples));
+        assertEquals(93, new FileScanner(this::check).scan(examples));
         assertEquals(43, testCasesInFiles);
     }
 
