bad4a92d107cf60f4453519a028d8e945194f902
hneemann <helmut.neemann@arcor.de>
2017-02-24 13:38:48 +0100

    added an cmos or gate

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -28,8 +28,8 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(98, new FileScanner(this::check).scan(examples));
-        assertEquals(50, testCasesInFiles);
+        assertEquals(99, new FileScanner(this::check).scan(examples));
+        assertEquals(51, testCasesInFiles);
     }
 
     /**
