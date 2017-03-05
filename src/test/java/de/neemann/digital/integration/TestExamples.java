5932987a2033fa452d962afe9bbf8a2733a37b65
hneemann <helmut.neemann@arcor.de>
2017-03-05 21:36:04 +0100

    Fixed some bug concerning switches added a 16-Bit SRAM example

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -29,7 +29,7 @@ public class TestExamples extends TestCase {
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
         assertEquals(100, new FileScanner(this::check).scan(examples));
-        assertEquals(51, testCasesInFiles);
+        assertEquals(52, testCasesInFiles);
     }
 
     /**
