36782ec46f830965a13d9c6f7d5edb2137885791
hneemann <helmut.neemann@arcor.de>
2017-03-08 14:21:06 +0100

    added some probe naming tests.

---
+++ src/test/java/de/neemann/digital/core/io/ProbeTest.java
@@ -0,0 +1,34 @@
+package de.neemann.digital.core.io;
+
+import de.neemann.digital.core.Model;
+import de.neemann.digital.core.Signal;
+import de.neemann.digital.integration.ToBreakRunner;
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+
+/**
+ * Make sure that probes are correctly named and added to the list of signals.
+ * Created by hneemann on 08.03.17.
+ */
+public class ProbeTest extends TestCase {
+
+    public void testProbe() throws Exception {
+        Model m = new ToBreakRunner("dig/probe/probe.dig").getModel();
+        ArrayList<Signal> signals = m.getSignals();
+        assertEquals(5, signals.size());
+
+        assertTrue(signals.contains(new Signal("M_Probe1", null)));
+        assertTrue(signals.contains(new Signal("M_Probe2", null)));
+    }
+
+    public void testProbeNesting() throws Exception {
+        Model m = new ToBreakRunner("dig/probe/probeNest.dig").getModel();
+        ArrayList<Signal> signals = m.getSignals();
+        assertEquals(6, signals.size());
+
+        assertTrue(signals.contains(new Signal("M_Probe1", null)));
+        assertTrue(signals.contains(new Signal("M_Nest_Probe1", null)));
+        assertTrue(signals.contains(new Signal("M_Nest_Probe2", null)));
+    }
+}
