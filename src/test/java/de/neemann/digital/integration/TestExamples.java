acdb37945dfa6f22f2377a96725c2dde0057b2d9
hneemann <helmut.neemann@arcor.de>
2017-05-13 12:39:42 +0200

    added some 74xx circuits

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -28,8 +28,8 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(129, new FileScanner(this::check).scan(examples));
-        assertEquals(73, testCasesInFiles);
+        assertEquals(137, new FileScanner(this::check).scan(examples));
+        assertEquals(74, testCasesInFiles);
     }
 
     /**
