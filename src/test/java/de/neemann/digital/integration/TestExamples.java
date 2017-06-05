42505804f9b64cb350714ff5ad00473911d3a089
hneemann <helmut.neemann@arcor.de>
2017-06-05 10:39:55 +0200

    added a reverse order splitter port test case

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -39,8 +39,8 @@ public class TestExamples extends TestCase {
      */
     public void testTestExamples() throws Exception {
         File examples = new File(Resources.getRoot(), "/dig/test");
-        assertEquals(71, new FileScanner(this::check).scan(examples));
-        assertEquals(65, testCasesInFiles);
+        assertEquals(72, new FileScanner(this::check).scan(examples));
+        assertEquals(66, testCasesInFiles);
     }
 
 
