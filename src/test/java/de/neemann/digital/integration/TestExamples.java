32da3e7d9bb6e8cd6e292b6e4ad50a561791062d
hneemann <helmut.neemann@arcor.de>
2017-07-09 20:53:52 +0200

    added nmos xor example

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -28,8 +28,8 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(188, new FileScanner(this::check).scan(examples));
-        assertEquals(87, testCasesInFiles);
+        assertEquals(190, new FileScanner(this::check).scan(examples));
+        assertEquals(89, testCasesInFiles);
     }
 
     /**
