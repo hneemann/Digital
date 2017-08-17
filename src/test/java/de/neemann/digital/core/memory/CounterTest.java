45e30728a6b68732f90cf9694d8b21a8cd3e591d
hneemann <helmut.neemann@arcor.de>
2017-08-17 14:39:27 +0200

    Added enable input to counter and added vhdl template for the counter.

--- src/test/java/de/neemann/digital/core/memory/CounterTest.java
+++ src/test/java/de/neemann/digital/core/memory/CounterTest.java
@@ -15,33 +15,34 @@ public class CounterTest extends TestCase {
 
     public void testCounter() throws Exception {
         ObservableValue clk = new ObservableValue("clk", 1);
+        ObservableValue en = new ObservableValue("en", 1);
         ObservableValue clr = new ObservableValue("clr", 1);
 
         Model model = new Model();
         Counter out = model.add(new Counter(
                 new ElementAttributes()
                         .setBits(8)));
-        out.setInputs(ovs(clk, clr));
+        out.setInputs(ovs(clk, en, clr));
 
-        TestExecuter sc = new TestExecuter(model).setInputs(clk, clr).setOutputs(out.getOutputs());
-        sc.check(0, 0, 0, 0);
-        sc.check(1, 0, 1, 0);
-        sc.check(0, 0, 1, 0);
-        sc.check(1, 0, 2, 0);
-        sc.check(0, 0, 2, 0);
-        sc.check(1, 0, 3, 0);
-        sc.check(0, 0, 3, 0);
-        sc.check(0, 1, 0, 0);
+        TestExecuter sc = new TestExecuter(model).setInputs(clk, en, clr).setOutputs(out.getOutputs());
+        sc.check(0, 1, 0, 0, 0);
+        sc.check(1, 1, 0, 1, 0);
+        sc.check(0, 1, 0, 1, 0);
+        sc.check(1, 1, 0, 2, 0);
+        sc.check(0, 1, 0, 2, 0);
+        sc.check(1, 1, 0, 3, 0);
+        sc.check(0, 1, 0, 3, 0);
+        sc.check(0, 1, 1, 0, 0);
 
-        sc.check(0, 0, 0, 0);
+        sc.check(0, 1, 0, 0, 0);
         for (int i = 1; i <= 255; i++) {
-            sc.check(0, 0, i - 1, 0);
-            sc.check(1, 0, i, 0);
+            sc.check(0, 1, 0, i - 1, 0);
+            sc.check(1, 1, 0, i, 0);
         }
-        sc.check(0, 0, 255, 0);
-        sc.check(1, 0, 0, 1);
-        sc.check(0, 0, 0, 1);
-        sc.check(1, 0, 1, 0);
+        sc.check(0, 1, 0, 255, 0);
+        sc.check(1, 1, 0, 0, 1);
+        sc.check(0, 1, 0, 0, 1);
+        sc.check(1, 1, 0, 1, 0);
     }
 
 
