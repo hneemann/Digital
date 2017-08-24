cebfb2bef85bdabbe2a47ea118c2d8f049f557d0
guiweber <guillaume.web@gmail.com>
2017-08-24 11:54:05 -0400

    Added 74109

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -29,8 +29,8 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(198, new FileScanner(this::check).scan(examples));
-        assertEquals(98, testCasesInFiles);
+        assertEquals(199, new FileScanner(this::check).scan(examples));
+        assertEquals(100, testCasesInFiles);
     }
 
     /**
