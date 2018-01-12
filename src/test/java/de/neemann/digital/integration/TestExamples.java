06be73ce02cf9b87b053af3e01396334badecba8
hneemann <helmut.neemann@arcor.de>
2018-01-12 09:20:16 +0100

    added a 16 bit multiplier example

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -29,8 +29,8 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(203, new FileScanner(this::check).scan(examples));
-        assertEquals(108, testCasesInFiles);
+        assertEquals(204, new FileScanner(this::check).scan(examples));
+        assertEquals(109, testCasesInFiles);
     }
 
     /**
