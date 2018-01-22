1761959e5fb184d350833fd2303f59901fe3867e
hneemann <helmut.neemann@arcor.de>
2018-01-22 08:40:01 +0100

    added some more cmos examples.

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -29,8 +29,8 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(205, new FileScanner(this::check).scan(examples));
-        assertEquals(109, testCasesInFiles);
+        assertEquals(208, new FileScanner(this::check).scan(examples));
+        assertEquals(112, testCasesInFiles);
     }
 
     /**
