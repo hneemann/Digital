915c2adb0e45826708e58336205f40b810eed688
hneemann <helmut.neemann@arcor.de>
2018-02-02 09:10:48 +0100

    modified some test cases

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -41,7 +41,7 @@ public class TestExamples extends TestCase {
     public void testTestExamples() throws Exception {
         File examples = new File(Resources.getRoot(), "/dig/test");
         assertEquals(121, new FileScanner(this::check).scan(examples));
-        assertEquals(109, testCasesInFiles);
+        assertEquals(110, testCasesInFiles);
     }
 
 
