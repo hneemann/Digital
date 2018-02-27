07603fe5d52586763226e5c539bae4956dc7ff11
hneemann <helmut.neemann@arcor.de>
2018-02-27 11:26:20 +0100

    added some EPROMs to the library

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -29,7 +29,7 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(216, new FileScanner(this::check).scan(examples));
+        assertEquals(222, new FileScanner(this::check).scan(examples));
         assertEquals(160, testCasesInFiles);
     }
 
