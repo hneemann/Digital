bc6bbbeae8afeaabca05ee52b786843e5a191031
hneemann <helmut.neemann@arcor.de>
2018-03-03 13:55:29 +0100

    added 74670

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -32,7 +32,7 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(232, new FileScanner(this::check).scan(examples));
+        assertEquals(233, new FileScanner(this::check).scan(examples));
         assertEquals(160, testCasesInFiles);
     }
 
