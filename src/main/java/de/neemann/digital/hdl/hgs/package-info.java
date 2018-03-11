f5805a1717cda588e432e362a5ea3e2790820620
Ivan de Jesus Deras <ideras@gmail.com>
2018-03-11 09:43:54 -0600

    Merge with upstream Digital March 11, 2018.  Added the GPL license notice to some files that were missing it.

--- src/main/java/de/neemann/digital/hdl/hgs/package-info.java
+++ src/main/java/de/neemann/digital/hdl/hgs/package-info.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018 Ivan Deras
+ * Use of this source code is governed by the GPL v3 license
+ * that can be found in the LICENSE file.
+ */
+
 /**
  * Classes to parse HG Script (HDL Generator Scripting language).
  * HG Script is a simple scripting language similar to PHP, which we use here to generate Verilog
--- src/main/java/de/neemann/digital/hdl/verilog/lib/package-info.java
+++ src/main/java/de/neemann/digital/hdl/verilog/lib/package-info.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018 Ivan Deras
+ * Use of this source code is governed by the GPL v3 license
+ * that can be found in the LICENSE file.
+ */
+
 /**
  * The Verilog library.
  * Here you can find the code generators for the components
