ed097c74f29d11bb514667d6f0e558c933499f87
Felix Schütze <dev@felix-schuetze.de>
2018-04-24 09:36:50 +0200

    Pinnable Interface

--- src/main/java/de/neemann/digital/draw/graphics/svg/SVGPinnable.java
+++ src/main/java/de/neemann/digital/draw/graphics/svg/SVGPinnable.java
@@ -7,12 +7,14 @@ package de.neemann.digital.draw.graphics.svg;
 
 /**
  * Interface to identify a Fragment which may habe a Pin inside
+ * 
  * @author felix
  */
 public interface SVGPinnable {
-    /**
-     * Gets the Pins
-     * @return pins
-     */
-    SVGPseudoPin[] getPin();
+	/**
+	 * Gets the Pins
+	 * 
+	 * @return pins
+	 */
+	SVGPseudoPin[] getPin();
 }
