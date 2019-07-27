7c08acfea49b0a365be9194c0ed734f11b6967a5
hneemann <helmut.neemann@arcor.de>
2019-07-27 20:13:04 +0200

    added integration test for LaTeX export.

--- src/test/java/de/neemann/digital/gui/components/table/LaTeXExpressionListenerTest.java
+++ src/test/java/de/neemann/digital/gui/components/table/LaTeXExpressionListenerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Helmut Neemann
+ * Copyright (c) 2019 Helmut Neemann
  * Use of this source code is governed by the GPL v3 license
  * that can be found in the LICENSE file.
  */
