b0eb8726f3eff4bcd5905fe30499bf00fa5dba4b
hneemann <helmut.neemann@arcor.de>
2019-08-12 17:55:38 +0200

    adds two new 7-seg drivers, closes #305

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -32,8 +32,8 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(269, new FileScanner(this::check).scan(examples));
-        assertEquals(186, testCasesInFiles);
+        assertEquals(271, new FileScanner(this::check).scan(examples));
+        assertEquals(188, testCasesInFiles);
     }
 
     /**
