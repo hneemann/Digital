9773df8c15e1e4e334737c453edc950e96b7cff8
Michael Schaar <kraeml@users.noreply.github.com>
2019-09-11 12:27:07 +0200

    Realpath for Linux bash-script (#331)
    Bash script execute with real path of the script

--- distribution/Digital.sh
+++ distribution/Digital.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+DIR="$( cd "$( dirname "$( realpath "${BASH_SOURCE[0]}" )" )" >/dev/null && pwd )"
 java -jar $DIR/Digital.jar "$1"
