83126adb1b408b958ac818ed4812b56c02f4571e
hneemann <helmut.neemann@arcor.de>
2019-09-24 20:36:31 +0200

    added a 64 bit counter overflow test

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -43,8 +43,8 @@ public class TestExamples extends TestCase {
      */
     public void testTestExamples() throws Exception {
         File examples = new File(Resources.getRoot(), "/dig/test");
-        assertEquals(183, new FileScanner(this::check).scan(examples));
-        assertEquals(171, testCasesInFiles);
+        assertEquals(184, new FileScanner(this::check).scan(examples));
+        assertEquals(172, testCasesInFiles);
     }
 
     /**
