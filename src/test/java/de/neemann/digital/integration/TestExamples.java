ee046ab4d13adfdfe3a5ba28292bcfee9505d9be
hneemann <helmut.neemann@arcor.de>
2019-10-20 09:51:43 +0200

    adds the 74595, closes #360

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -32,7 +32,7 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(281, new FileScanner(this::check).scan(examples));
+        assertEquals(282, new FileScanner(this::check).scan(examples));
         assertEquals(191, testCasesInFiles);
     }
 
