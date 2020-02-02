aba1ecf80484531acf16434744668cad35931022
hneemann <helmut.neemann@arcor.de>
2020-02-02 09:14:08 +0100

    adds 74193, closes #416

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -32,8 +32,8 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(290, new FileScanner(this::check).scan(examples));
-        assertEquals(192, testCasesInFiles);
+        assertEquals(291, new FileScanner(this::check).scan(examples));
+        assertEquals(193, testCasesInFiles);
     }
 
     /**
