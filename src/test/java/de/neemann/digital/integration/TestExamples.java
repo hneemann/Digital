ec08edbcdaeb8a8c1d6ce07ba50c7d27f2ed459c
hneemann <helmut.neemann@arcor.de>
2020-09-17 10:14:51 +0200

    adds a Dadda multiplier example

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -33,8 +33,8 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(299, new FileScanner(this::check).scan(examples));
-        assertEquals(195, testCasesInFiles);
+        assertEquals(300, new FileScanner(this::check).scan(examples));
+        assertEquals(196, testCasesInFiles);
     }
 
     /**
