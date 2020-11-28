51ba9034b7bcf347a1c83ddae47f85f8992a07d1
hneemann <helmut.neemann@arcor.de>
2020-11-28 20:23:55 +0100

    adds the new auto generated processor test

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -39,8 +39,8 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(302, new FileScanner(this::check).scan(examples));
-        assertEquals(203, testCasesInFiles);
+        assertEquals(301, new FileScanner(this::check).scan(examples));
+        assertEquals(233, testCasesInFiles);
     }
 
     /**
