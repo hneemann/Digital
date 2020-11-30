11494ca1f37c467499a37dde550f43e0a0b37c0d
hneemann <helmut.neemann@arcor.de>
2020-11-30 09:11:57 +0100

    adds processor tests for io instructions

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -39,8 +39,8 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(301, new FileScanner(this::check).scan(examples));
-        assertEquals(325, testCasesInFiles);
+        assertEquals(302, new FileScanner(this::check).scan(examples));
+        assertEquals(331, testCasesInFiles);
     }
 
     /**
