2af009a670570f61e71d69002783b51d19c733af
Mats Engstrom <mats.engstrom@gmail.com>
2021-04-09 11:31:47 +0200

    Fix unused include

--- src/main/java/de/neemann/digital/draw/shapes/CntProbeShape.java
+++ src/main/java/de/neemann/digital/draw/shapes/CntProbeShape.java
@@ -5,7 +5,6 @@
  */
 package de.neemann.digital.draw.shapes;
 
-import de.neemann.digital.core.Observer;
 import de.neemann.digital.core.element.ElementAttributes;
 import de.neemann.digital.core.element.PinDescriptions;
 import de.neemann.digital.draw.elements.IOState;
