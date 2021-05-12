1fe7b8db0347a959d2628261d8014bb1386bcd5d
hneemann <helmut.neemann@arcor.de>
2021-05-12 10:21:01 +0200

    adds a telnet example

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -40,7 +40,7 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(316, new FileScanner(this::check).scan(examples));
+        assertEquals(317, new FileScanner(this::check).scan(examples));
         assertEquals(502, testCasesInFiles);
     }
 
