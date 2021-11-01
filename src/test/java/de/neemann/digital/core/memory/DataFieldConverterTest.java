c24fd8588d6088fc6feb023c0b504574fdf19595
FanYuliang <yf3@illinois.edu>
2021-11-01 00:13:09 -0500

    refactor common parts into checkequals

--- src/test/java/de/neemann/digital/core/memory/DataFieldConverterTest.java
+++ src/test/java/de/neemann/digital/core/memory/DataFieldConverterTest.java
@@ -8,7 +8,6 @@ package de.neemann.digital.core.memory;
 import com.thoughtworks.xstream.XStream;
 import de.neemann.digital.XStreamValid;
 import junit.framework.TestCase;
-import java.util.*;
 /**
  *
  */
@@ -53,6 +52,12 @@ public class DataFieldConverterTest extends TestCase {
         }
     }
 
+    void checkEquals(String prefix, String d1, String d2, String suffix, String xml) {
+         String case1 = prefix + d1 + d2 + suffix;
+         String case2 = prefix + d2 + d1 + suffix;
+         assertTrue(case1.equals(xml) || case2.equals(xml));
+    }
+
     public void testMarshalObj() {
         final DataField d1 = new DataField(20);
         d1.setData(0, 1);
@@ -65,15 +70,12 @@ public class DataFieldConverterTest extends TestCase {
         XStream xs = getxStream();
         String xml = xs.toXML(new Test(d1, d2));
         
-        String case1 = "<?xml version=\"1.0\" ?><test>" + 
-            "<d1>1,4*0,2</d1>" + 
-            "<d2>3,7*0,4</d2>" + 
-            "</test>";
-        String case2 = "<?xml version=\"1.0\" ?><test>" + 
-            "<d2>3,7*0,4</d2>" + 
-            "<d1>1,4*0,2</d1>" + 
-            "</test>";
-        assertTrue(Arrays.asList(case1, case2).contains(xml));
+        String prefix = "<?xml version=\"1.0\" ?><test>";
+        String d1_str = "<d1>1,4*0,2</d1>";
+        String d2_str = "<d2>3,7*0,4</d2>";
+        String suffix = "</test>";
+
+        checkEquals(prefix, d1_str, d2_str, suffix, xml);
     }
 
     public void testMarshalObj2() {
@@ -86,15 +88,12 @@ public class DataFieldConverterTest extends TestCase {
         XStream xs = getxStream();
         String xml = xs.toXML(new Test(d1, d2));
 
-        String case1 = "<?xml version=\"1.0\" ?><test>" + 
-            "<d1>5*0,2</d1>" + 
-            "<d2>8*0,4</d2>" + 
-            "</test>";
-        String case2 = "<?xml version=\"1.0\" ?><test>" + 
-            "<d2>8*0,4</d2>" + 
-            "<d1>5*0,2</d1>" + 
-            "</test>";
-        assertTrue(Arrays.asList(case1, case2).contains(xml));
+        String prefix = "<?xml version=\"1.0\" ?><test>";
+        String d1_str = "<d1>5*0,2</d1>";
+        String d2_str = "<d2>8*0,4</d2>";
+        String suffix = "</test>";
+
+        checkEquals(prefix, d1_str, d2_str, suffix, xml);
     }
 
     public void testMarshalObj3() {
@@ -109,16 +108,13 @@ public class DataFieldConverterTest extends TestCase {
 
         XStream xs = getxStream();
         String xml = xs.toXML(new Test(d1, d2));
+        
+        String prefix = "<?xml version=\"1.0\" ?><test>";
+        String d1_str = "<d1>11*2</d1>";
+        String d2_str = "<d2>11*1,11*2,11*3,11*4,11*5,11*6,11*7,11*8,11*9,11*a,11*b</d2>";
+        String suffix = "</test>";
 
-        String case1 = "<?xml version=\"1.0\" ?><test>" + 
-            "<d1>11*2</d1>" + 
-            "<d2>11*1,11*2,11*3,11*4,11*5,11*6,11*7,11*8,11*9,11*a,11*b</d2>" + 
-            "</test>";
-        String case2 = "<?xml version=\"1.0\" ?><test>" + 
-            "<d2>11*1,11*2,11*3,11*4,11*5,11*6,11*7,11*8,11*9,11*a,11*b</d2>" + 
-            "<d1>11*2</d1>" + 
-            "</test>";
-        assertTrue(Arrays.asList(case1, case2).contains(xml));
+        checkEquals(prefix, d1_str, d2_str, suffix, xml);
     }
 
     public void testUnmarshalObj() {
