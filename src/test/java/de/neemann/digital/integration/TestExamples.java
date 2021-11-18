0d9ae65a7c88fe06666506e4d18e7ac4682cbdba
hneemann <helmut.neemann@arcor.de>
2021-11-18 07:34:25 +0100

    adds the 7490 counter, closes #877

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -40,8 +40,8 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(317, new FileScanner(this::check).scan(examples));
-        assertEquals(502, testCasesInFiles);
+        assertEquals(318, new FileScanner(this::check).scan(examples));
+        assertEquals(507, testCasesInFiles);
     }
 
     /**
