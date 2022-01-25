a351210b2eb928420c19bf4b901d82e82e4aad7f
hneemann <helmut.neemann@arcor.de>
2022-01-25 07:55:52 +0100

    adds the 7493 chip, closes #899

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -40,7 +40,7 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(318, new FileScanner(this::check).scan(examples));
+        assertEquals(319, new FileScanner(this::check).scan(examples));
         assertEquals(507, testCasesInFiles);
     }
 
