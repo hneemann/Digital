08bb5be2927080a1c16972e89a3fe12ac9abc8bc
pavels.korzs <pavels.korzs@codex.lv>
2023-09-13 23:12:48 +0300

    PinControl has hardcoded bit amount (2) in case of multi bite PinControl fully ignore Z statuses Uses PinControl in "inout" verilog models for UnitTest for satisfy export to verilog.

--- src/main/resources/verilog/DIG_PinControl.v
+++ src/main/resources/verilog/DIG_PinControl.v
@@ -1,18 +1,15 @@
 <?
     if (elem.Bits > 1) {
-        generics[0] := "Bits";
         export bitRange := "[(Bits-1):0] ";
-        export zval := "{Bits{1'bz}}";
     }
     else {
-        moduleName = moduleName+"_BUS";
         export bitRange := "";
-        export zval := "1'bz";
     }
+    export zval := elem.Bits+"'bz";
 ?>module <?= moduleName ?><?
 if (elem.Bits > 1) { ?>
 #(
-    parameter Bits = 2
+    parameter Bits = <?=elem.Bits?>
 )
 <? } ?>(
    inout <?= bitRange ?>pin,
