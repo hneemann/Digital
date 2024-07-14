7e3ebcdf035324902c06d675aa88bf1321b8f1de
hneemann <helmut.neemann@arcor.de>
2024-07-14 09:21:16 +0200

    added 7434, 74381 and 747266. Closes #1298, closes #1297, closes #1296

--- src/test/java/de/neemann/digital/integration/TestExamples.java
+++ src/test/java/de/neemann/digital/integration/TestExamples.java
@@ -40,8 +40,8 @@ public class TestExamples extends TestCase {
      */
     public void testDistExamples() throws Exception {
         File examples = new File(Resources.getRoot().getParentFile().getParentFile(), "/main/dig");
-        assertEquals(327, new FileScanner(this::check).scan(examples));
-        assertEquals(521, testCasesInFiles);
+        assertEquals(330, new FileScanner(this::check).scan(examples));
+        assertEquals(522, testCasesInFiles);
     }
 
     /**
