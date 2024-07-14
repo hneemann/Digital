426a1e11700c8a6e3dfd96a49a1fdba5389ec05d
hneemann <helmut.neemann@arcor.de>
2024-07-14 09:57:16 +0200

    Highlights the input connection port in the multiplexer when the input is selected; closes #1299

--- src/main/java/de/neemann/digital/draw/shapes/MuxerShape.java
+++ src/main/java/de/neemann/digital/draw/shapes/MuxerShape.java
@@ -5,6 +5,8 @@
  */
 package de.neemann.digital.draw.shapes;
 
+import de.neemann.digital.core.ObservableValue;
+import de.neemann.digital.core.Value;
 import de.neemann.digital.core.element.ElementAttributes;
 import de.neemann.digital.core.element.Keys;
 import de.neemann.digital.core.element.PinDescriptions;
@@ -14,6 +16,7 @@ import de.neemann.digital.draw.elements.Pins;
 import de.neemann.digital.draw.graphics.*;
 
 import static de.neemann.digital.draw.shapes.GenericShape.SIZE;
+import static de.neemann.digital.draw.shapes.GenericShape.SIZE2;
 
 /**
  * The Muxer shape
@@ -24,6 +27,8 @@ public class MuxerShape implements Shape {
     private final PinDescriptions inputs;
     private final PinDescriptions outputs;
     private Pins pins;
+    private ObservableValue selector;
+    private Value selectorValue;
 
     /**
      * Creates a new instance
@@ -58,7 +63,14 @@ public class MuxerShape implements Shape {
 
     @Override
     public Interactor applyStateMonitor(IOState ioState) {
+        selector = ioState.getInput(0);
         return null;
+    }
+
+    @Override
+    public void readObservableValues() {
+        if (selector != null)
+            selectorValue = selector.getCopy();
     }
 
     @Override
@@ -69,5 +81,15 @@ public class MuxerShape implements Shape {
                 .add(SIZE * 2 - 1, inputCount * SIZE - 5)
                 .add(1, inputCount * SIZE + 4), Style.NORMAL);
         graphic.drawText(new Vector(3, 2), "0", Orientation.LEFTTOP, Style.SHAPE_PIN);
+
+        if (selectorValue != null) {
+            int in = (int) selectorValue.getValue() + 1;
+            Pins p = getPins();
+            if (in < p.size()) {
+                Vector pos = p.get(in).getPos();
+                int s = SIZE2 / 2;
+                graphic.drawCircle(pos.add(-s, -s), pos.add(s, s), Style.THIN);
+            }
+        }
     }
 }
