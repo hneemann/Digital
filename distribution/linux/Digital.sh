0ec379087ef51139ada21b23e4a8ffb1c2db31a1
sjneves <jneves42@hotmail.com>
2024-10-26 17:31:29 -0300

    Modified Digital.sh to add some environment variables for executing in a Wayland session using XWayland

--- distribution/linux/Digital.sh
+++ distribution/linux/Digital.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "$( realpath "${BASH_SOURCE[0]}" )" )" >/dev/null && pwd )"
+export JAVA_TOOL_OPTIONS='-Djava.awt.headless=false'
 java -jar "$DIR/Digital.jar" "$1"
