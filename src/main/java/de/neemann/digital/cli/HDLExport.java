d60ac5233fa0300c59d3b85892303d61bff7c107
jeremiahhidayat <Jeremiahhidayat@yahoo.com>
2026-02-16 16:02:08 -0500

    finish HDLexport implentation

--- src/main/java/de/neemann/digital/cli/HDLExport.java
+++ src/main/java/de/neemann/digital/cli/HDLExport.java
@@ -6,13 +6,13 @@
 package de.neemann.digital.cli;
 
 import java.io.File;
-import java.io.IOException;
 
 import de.neemann.digital.cli.cli.Argument;
 import de.neemann.digital.cli.cli.BasicCommand;
 import de.neemann.digital.cli.cli.CLIException;
 import de.neemann.digital.hdl.printer.CodePrinter;
 import de.neemann.digital.hdl.verilog2.VerilogGenerator;
+import de.neemann.digital.hdl.vhdl2.VHDLGenerator;
 import de.neemann.digital.lang.Lang;
 
 /**
@@ -20,8 +20,8 @@ import de.neemann.digital.lang.Lang;
  */
 public class HDLExport extends BasicCommand {
     private final Argument<String> digFile;
-    private final Argument<String> HDLFile;
-    private final Argument<String> langugage;
+    private final Argument<String> hdlFile;
+    private final Argument<String> language;
 
     /**
      * Creates the run command
@@ -29,52 +29,62 @@ public class HDLExport extends BasicCommand {
     public HDLExport() {
         super("export");
         digFile = addArgument(new Argument<>("dig", "", false));
-        HDLFile = addArgument(new Argument<>("hdl", "", true));
-        langugage = addArgument(new Argument<>("language", "", false));
+        hdlFile = addArgument(new Argument<>("hdl", "", true));
+        language = addArgument(new Argument<>("language", "", false));
     }
 
     @Override
     public void execute() throws CLIException {
 
         String digFilePath = digFile.get();
-        String lang = langugage.get().toLowerCase();
+        String lang = language.get().toLowerCase(); // normalize string input
 
         // Validate the language
         if (!lang.equals("verilog") && !lang.equals("vhdl")) {
-            throw new CLIException(Lang.get("cli_errorRunningCircuit"), e);
+            throw new CLIException(Lang.get("cli_errorLanguageInvalid"), null);
         }
 
-        // Determine output file
-        File digFileObj = new File(digFilePath);
+        // Validate the dig file
+        File digFileObj = new File(digFile.get());
+        if (!digFileObj.exists()) {
+            throw new CLIException(Lang.get("cli_errorDigFileNotFound"), null);
+        }
+
+        // Determine output file based on arguments, default is dig name/path converted
+        // to HDL
         String digFileName = digFileObj.getName();
         String digDir = digFileObj.getParent();
-
         File hdlFileObj;
-        if (HDLFile.isSet()) {
-            hdlFileObj = new File(HDLFile.get());
+        if (hdlFile.isSet()) {
+            hdlFileObj = new File(hdlFile.get());
         } else {
-            String baseName = digFileName;
-            if (digFileName.endsWith(".dig")) {
-                baseName = digFileName.substring(0, digFileName.length() - 4);
-            }
-
-            if (langugage.get() == "verilog") {
-                String defaultHDLFileName = baseName + ".v"; // default to Verilog
-            } else if (langugage.get() == "vhdl") {
-                String defaultHDLFileName = baseName + ".vhd"; // default to Verilog
-            }
-
+            String baseName = digFileName.replaceAll("\\.dig$", "");
+            String defaultHDLFileName = lang.equals("verilog") ? baseName + ".v" : baseName + ".vhd";
             hdlFileObj = new File(digDir, defaultHDLFileName);
         }
 
         try {
             CircuitLoader circuitLoader = new CircuitLoader(digFilePath, false);
-            VerilogGenerator verilogGenerator = new VerilogGenerator(circuitLoader.getLibrary(),
-                    new CodePrinter(verilogFile));
-            verilogGenerator.export(circuitLoader.getCircuit());
-            System.out.println("Verilog export completed successfully.");
-        } catch (IOException e) {
-            throw new CLIException("Failed to export Verilog: " + e.getMessage(), e);
+
+            if (lang.equals("verilog")) {
+                try (
+                        VerilogGenerator verilogGenerator = new VerilogGenerator(
+                                circuitLoader.getLibrary(),
+                                new CodePrinter(hdlFileObj))) {
+                    verilogGenerator.export(circuitLoader.getCircuit());
+                }
+            } else {
+                try (VHDLGenerator vhdlGenerator = new VHDLGenerator(
+                        circuitLoader.getLibrary(),
+                        new CodePrinter(hdlFileObj))) {
+                    vhdlGenerator.export(circuitLoader.getCircuit());
+                }
+            }
+
+            System.out.println("Export completed successfully to " + hdlFileObj.getAbsolutePath());
+        } catch (Exception e) {
+            e.printStackTrace(); // <-- ADD THIS
+            throw new CLIException(Lang.get("cli_errorExportFailed"), e);
         }
     }
 }
